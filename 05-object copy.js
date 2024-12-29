aac833028eefd7996bf57a716a24f4346a9d0282
Queenie Chau <tosbackpacker@yahoo.com>
2024-12-29 22:50:44 +0800

    let user.id for differet users

---
+++ 05-object copy.js
@@ -0,0 +1,8 @@
+let user = {}
+user.id = 10
+user.name = 'Alice'
+
+console.log(user.id + 400)
+
+// console.log('user' + user.id) > must use ' ' outside user
+console.log('user' + user.id + ' is ' + user.name)// must use ' ' outside user
